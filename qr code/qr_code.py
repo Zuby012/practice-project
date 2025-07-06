11f1b8b585edd42129c9053c3c598c74136f20a6
Zuby012 <amahblaise0@gmail.com>
2025-07-06 23:55:06 +0100

    qr code practice
    a practice project for the generation of QR codes in python

---
+++ qr code/qr_code.py
@@ -0,0 +1,17 @@
+import qrcode
+import qrcode.constants
+
+text = input("URL: ")
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=15,
+    border=2
+)
+
+qr.add_data(text)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("my_qrcode.png")
