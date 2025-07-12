ea3c30c4741bd35f068ecc9a55bf237e815933b0
Zuby012 <amahblaise0@gmail.com>
2025-07-12 20:19:53 +0100

    Update simple_chatbot.py

--- chat bot/simple_chatbot.py
+++ chat bot/simple_chatbot.py
@@ -9,10 +9,24 @@ knowledge_base = {
     "help": "You can ask me about my name, say hello, or say bye."
 }
 
+user_correction = "x"  # just a placeholder for the user_correction variable
+
+# function to add new request and response to knowledge_base
+
+
+def add_new_content(user_input, user_correction):
+    user_correction = input(
+        "--Would you help by telling me the correct response: ")
+    knowledge_base[user_input] = user_correction
+
+# function to filter every punctuation from request before getting response
+
 
 def remove_punctuation(text: str) -> str:
     cleaned = re.sub(r'[^a-zA-Z0-9\s]', '', text)
     return cleaned
+
+# function to get response after removing punctuations
 
 
 def get_response(user_input: str) -> str:
@@ -20,12 +34,14 @@ def get_response(user_input: str) -> str:
     for key in knowledge_base:
         if key in cleaned_input:
             return knowledge_base[key]
-    return "Sorry, I don't understand."
+    print("Sorry i don't understand")
+    add_new_content(user_input, user_correction)
+    return "Thank you."
 
 
 print("Chatbot: Hello! Type 'bye' to exit.")
 while True:
-    user_input = input("You: ")
+    user_input = input("--You: ")
     if user_input.lower() == "bye":
         print("Chatbot: " + knowledge_base["bye"])
         break
